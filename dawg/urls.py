47e2236544a9c6a72fac3d5a7a26c0b83f0a3cca
shanekeran <shanekeran@gmail.com>
2021-06-26 12:55:24 +0000

    added home app and templates

--- dawg/urls.py
+++ dawg/urls.py
@@ -1,4 +1,4 @@
-"""dawg URL Configuration
+"""Dawg URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]

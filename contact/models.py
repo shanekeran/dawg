0cb3b2cd8ec12459d543480125103400cd7ed2fc
shanekeran <shanekeran@gmail.com>
2021-10-04 13:55:04 +0000

    Change CharField to EmailField on Contact form email field

--- contact/models.py
+++ contact/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     """Saves a contact model in database."""
 
     full_name = models.CharField(max_length=200, null=False, blank=False)
-    email_address = models.CharField(max_length=200, null=False, blank=False)
+    email_address = models.EmailField(max_length=200, null=False, blank=False)
     subject = models.CharField(max_length=50, null=False, blank=False)
     message = models.TextField()
     date_sent = models.DateTimeField(auto_now_add=True)

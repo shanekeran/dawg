5a5bd73e4101ddd36f83c974b7bf0225d747079f
shanekeran <shanekeran@gmail.com>
2021-10-05 15:58:37 +0000

    Add comments for better readability

--- contact/views.py
+++ contact/views.py
@@ -8,11 +8,14 @@ def contact_form(request):
 
     if request.method == 'POST':
         form = ContactUs(request.POST)
+        # Submits the form if inputted information is valid
         if form.is_valid():
             form.save()
+            # display success message
             messages.success(request, 'Dawg has received your message!')
             return redirect(reverse('home'))
         else:
+            # If form isn't valid, display error message.
             messages.error(request,
                             'Error. Review your submission and try again.')
     else:
